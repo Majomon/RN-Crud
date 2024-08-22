bd4fc3515550feebbb46bd9566bee4a274144dfc
Mauri <mauri.monzon91@gmail.com>
2024-08-22 20:51:33 -0300

    Producto por id

--- src/presentation/screens/show/ShowScreen.tsx
+++ src/presentation/screens/show/ShowScreen.tsx
@@ -1,6 +1,6 @@
 import {StackScreenProps} from '@react-navigation/stack';
 import React, {useEffect, useState} from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import {RootStackParams} from '../../navigation/StackNavigator';
 import {collection, getDocs, getFirestore} from 'firebase/firestore';
 import appFirebase from '../../../firebase/firebaseConfig';
@@ -10,22 +10,36 @@ const db = getFirestore(appFirebase);
 
 interface Props extends StackScreenProps<RootStackParams, 'ShowScreen'> {}
 
+interface Product {
+  name: string;
+  color: string;
+  stock: string;
+}
+
 export const ShowScreen = ({route}: Props) => {
-  const [product, setProduct] = useState({});
+  const [product, setProduct] = useState<Product | null>(null);
 
   useEffect(() => {
     getOneProduct(route.params.productId, setProduct);
 
-    console.log(product);
-    
     return () => {
-      setProduct({});
+      setProduct(null);
     };
   }, []);
 
   return (
-    <View>
-      <Text>ShowScreen</Text>
+    <View style={styles.container}>
+      <Text>Detalle de producto</Text>
+      <View style={{paddingVertical: 20}}>
+        <Text>Color: {product?.name}</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+});
